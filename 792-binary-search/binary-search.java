918ac38f37bc91dd81549145562c0ff8f234fc91
Ayush Shukla <nitianayush4141@gmail.com>
2026-08-31 10:27:17 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (91.85%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,18 +1,43 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//       int start = 0;
+//       int end = nums.length - 1;
+//       while(start<=end){
+//         int mid = start + (end - start)/2;
+//         if (target < nums[mid]){
+//             end = mid -1;
+//         }else if (target > nums[mid]){
+//             start = mid + 1;
+//         }else{
+//             return mid;
+//         }
+       
+//       } 
+//        return -1; 
+//     }
+// }
+                       //using recursive
 class Solution {
     public int search(int[] nums, int target) {
-      int start = 0;
-      int end = nums.length - 1;
-      while(start<=end){
-        int mid = start + (end - start)/2;
-        if (target < nums[mid]){
-            end = mid -1;
-        }else if (target > nums[mid]){
-            start = mid + 1;
-        }else{
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    static int binarySearch(int[] nums, int target, int start, int end) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target) {
             return mid;
         }
-       
-      } 
-       return -1; 
+
+        if (target < nums[mid]) {
+            return binarySearch(nums, target, start, mid - 1);
+        }
+
+        return binarySearch(nums, target, mid + 1, end);
     }
 }
